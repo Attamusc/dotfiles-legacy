d59e98c09247f8a15b085444e927344566bbd14b
Sean Dunn <sean@attamusc.com>
2012-09-01 18:08:47 -0400

    Added menu selection and colorization to completions for zsh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,7 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# use menu seletion for arrow key navigation and highlighting
+zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*' list-colors ''
