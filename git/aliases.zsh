f7eb753bd047279c41468ddcc3e70bca44819bd2
Sean Dunn <sean@attamusc.com>
2012-09-08 17:16:07 -0400

    Changed git aliases
    Most of the git aliases were set as aliases within the shell. I
    changed this so that now most aliases or aliases within the git
    command itself.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,19 +1,6 @@
 # Make git more awesome (if that's even possible)
 # Inspired by @holman's git-related dotfile and his
 # awesome "Git and GitHub Secrets" talk.
-alias gpull='git pull --prune'
-alias gl="git log --graph --pretty=format:'%C(bold red)%h%Creset %Cgreen(%cr)%Creset %C(blue)%an%Creset:%C(yellow)%d%Creset %s' --abbrev-commit --date=relative"
-alias gp='git push'
-alias gpom='git push origin master'
-alias gph='git push origin HEAD'
-alias gdi='git diff'
-alias gad='git add --all .'
-alias gc='git commit -m'
-alias gca='git commit -a -m'
-alias gco='git checkout'
-alias gcob='git checkout -b'
-alias gm='git merge --no-ff'
-alias gbr='git branch'
-alias gs='git status -sb'
+alias g='git'
 alias gcd='cd "`git rev-parse --show-toplevel`"'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
