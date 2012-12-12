cc86cbb27803a93019e19bf00bfe979be4483c39
Sean Dunn <sean@attamusc.com>
2012-12-11 22:57:51 -0500

    removes some conflicts

--- todo/todo.zsh
+++ todo/todo.zsh
@@ -1,5 +1,5 @@
 alias t='`brew --prefix`/bin/todo.sh -c -d $HOME/.todo.cfg'
-alias n='t ls +next'
+alias tn='t ls +next'
 
 _t() {
   local _todo_sh='`brew --prefix`/bin/todo.sh -d "$HOME/.todo.cfg"'
